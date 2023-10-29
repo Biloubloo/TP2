8395d98eaabbfba72e7566eadd1fd976f3bfa9c9
BilalBelkharraz <122588450+Biloubloo@users.noreply.github.com>
2023-10-29 15:43:29 +0000

    Ajout de commentaire

--- TP2/src/main/java/theatricalplays/StatementPrinter.java
+++ TP2/src/main/java/theatricalplays/StatementPrinter.java
@@ -8,12 +8,16 @@ import java.io.PrintWriter;
 
 public class StatementPrinter {
 
+
 public String print(Invoice invoice, Map<String, Play> plays) {
     int totalAmount = 0;
     int volumeCredits = 0;
+
+    // Utilisation d'un StringWriter pour générer la sortie
     StringWriter result = new StringWriter();
     PrintWriter out = new PrintWriter(result);
 
+    // En-tête du relevé de facturation
     out.format("Statement for %s\n", invoice.customer);
 
     NumberFormat frmt = NumberFormat.getCurrencyInstance(Locale.US);
@@ -22,6 +26,7 @@ public String print(Invoice invoice, Map<String, Play> plays) {
         Play play = plays.get(perf.playID);
         int thisAmount = 0;
 
+        // Calcul du montant pour cette performance en fonction du type de pièce
         switch (play.type) {
             case "tragedy":
                 thisAmount = 40000;
@@ -37,21 +42,27 @@ public String print(Invoice invoice, Map<String, Play> plays) {
                 thisAmount += 300 * perf.audience;
                 break;
             default:
-                throw new Error("unknown type: ${play.type}");
+                throw new Error("Unknown type: ${play.type}");
         }
 
-        // add volume credits
+        // Ajout de crédits de volume
         volumeCredits += Math.max(perf.audience - 30, 0);
-        // add extra credit for every ten comedy attendees
-        if ("comedy".equals(play.type)) volumeCredits += Math.floor(perf.audience / 5);
+        if ("comedy".equals(play.type)) {
+            // Ajout de crédits supplémentaires pour chaque tranche de 5 spectateurs dans une comédie
+            volumeCredits += Math.floor(perf.audience / 5);
+        }
 
-        // print line for this order
+        // Génération de la ligne pour cette performance
         out.format("  %s: %s (%s seats)\n", play.name, frmt.format(thisAmount / 100), perf.audience);
         totalAmount += thisAmount;
     }
+
+    // Ajout du montant total et des crédits de volume totaux
     out.format("Amount owed is %s\n", frmt.format(totalAmount / 100));
     out.format("You earned %d credits\n", volumeCredits);
 
+    // Conversion du StringWriter en chaîne de caractères
     return result.toString();
 }
 }
+
